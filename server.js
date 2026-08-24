16b06fc1d31c7fe7ef4b89ebc35b1269b67ffc17
Mehedi <text.rianbuft@gmail.com>
2026-08-25 03:43:25 +0600

    Add DATABASE_URL for Postgres

--- server.js
+++ server.js
@@ -409,5 +409,7 @@ app.delete("/api/orders/:id", async (req, res) => {
   }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
